1b228e3e985c78d9a211ddb65aa9ce0fcc7cd876
Gerardo Belot <gbelot2003@hotmail.com>
2024-02-11 23:55:59 -0600

    💄 cleaning cambas

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 )
@@ -9,43 +8,15 @@ import (
 const portNumber = ":8080"
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the home page")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	sum := addValues(2, 2)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is about pages and 2 + 2 is: %d", sum))
-}
-
-func Divide(w http.ResponseWriter, r *http.Request) {
-	f, err := divideValue(100.0, 10.0)
-
-	if err != nil {
-		fmt.Fprintf(w, "Cannot divide by 0")
-		return
-	}
-
-	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 10.0, f))
-}
-
-func divideValue(x, y float32) (float32, error) {
-	if y <= 0 {
-		err := errors.New("cannot divide by zero")
-		return 0, err
-	}
-	result := x / y
-	return result, nil
-}
-
-func addValues(x, y int) int {
-	return x + y
 }
 
 func main() {
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
